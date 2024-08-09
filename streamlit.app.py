2eb1efc5997510106391b02a538c66559a667da3
CarlosTamezEGOSBI <carlos.tamez@egosbi.com>
2024-08-09 12:07:02 -0600

    Create streamlit.app.py
    copied from my Custom Smoothies SiS app

---
+++ streamlit.app.py
@@ -0,0 +1,68 @@
+# Import python packages
+import streamlit as st
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+st.title(":cup_with_straw: Customize Your Smoothie:cup_with_straw:")
+st.write(
+    """Choose the fruits you want on your custom Smoothie!"""
+)
+ 
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+
+name_on_smoothie = st.text_input("Name on Smoothie:")
+st.write('"Choose the fruits your want in your custom Smoothie"')
+
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients:"
+    , my_dataframe
+    , max_selections=5
+)
+
+ingredients_string = ''
+vCount=0
+vInsert = False
+vRemaining = 0
+
+if ingredients_list:
+    for fruit_chosen in ingredients_list:
+        if vCount==4:
+            ingredients_string += fruit_chosen
+        elif vCount >= 5:
+            ingredients_string = ingredients_string
+        elif vCount ==0:
+            ingredients_string += fruit_chosen
+        else:
+            ingredients_string += ", " + fruit_chosen #fruit_chosen + ", " #"', '" 
+        vRemaining = 4 - vCount
+        vCount = vCount + 1 
+
+    if vRemaining >= 1:
+        st.write(str(vRemaining) + ' Ingredients Left.')
+        
+    if vCount > 5:
+        st.write('Just choose up to 5 ingredients. Please delete ' + str(vCount-5) + ' ingredients.')
+        vInsert = False
+    elif vCount<=5:
+        vInsert = True
+
+    if vInsert:
+        time_to_insert = st.button('Order here!')
+    else:
+        time_to_insert = False
+    
+    if time_to_insert:
+        if ingredients_string != "" and name_on_smoothie != "":
+            my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                                    values ('""" + ingredients_string + """', '""" + name_on_smoothie + """')"""
+
+            session.sql(my_insert_stmt).collect()
+            st.success('Your Smoothie is ordered!', icon="✅")
+        else:
+            if name_on_smoothie == "":
+                st.write("Please write a name on your smoothie.")
+                
+            if ingredients_string == "":
+                st.write("Choose your fruits befor order.")
