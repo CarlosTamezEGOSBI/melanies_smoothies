6057054f2612e71799afe330b4cf5849637deea4
CarlosTamezEGOSBI <carlos.tamez@egosbi.com>
2024-08-09 19:18:14 -0600

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -49,8 +49,8 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
-        #if search_on == ''
-            #search_on = 'None'
+        if search_on == ''
+            search_on = 'Vacio'
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
